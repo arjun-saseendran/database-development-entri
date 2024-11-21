f065254775c9c5f142bf853ae3e05b81e0481ec7
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-21 21:56:32 +0530

    add createTodo

--- src/controllers/todo.controllers.js
+++ src/controllers/todo.controllers.js
@@ -1,19 +1,21 @@
+import { Todo } from "../models/todo.models";
 
-const createTodo = async(req, res) => {
+const createTodo = async (req, res) => {
+const {todo} = req.body
+await Todo.create({todo})
 
+res.status(201).json({message: 'Created'})
 
-}
+};
 
 const readTodo = (req, res) => {
 
-}
+    
 
-const updateTodo = async (req, res) => {
+};
 
-}
+const updateTodo = async (req, res) => {};
 
-const deleteTodo = async(req, res) => {
+const deleteTodo = async (req, res) => {};
 
-}
-
-export {createTodo, readTodo, updateTodo, deleteTodo}
+export { createTodo, readTodo, updateTodo, deleteTodo };
