85aefde9c118443f68303b85bb80ee028ea0c9ef
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-22 12:25:30 +0530

    fix createTodo

--- src/controllers/todo.controllers.js
+++ src/controllers/todo.controllers.js
@@ -1,30 +1,32 @@
 import { Todo } from "../models/todo.models.js";
 
-const createTodo = async (req, res) => {
-const {todo} = req.body
-await Todo.create({todo})
+const createTodo = async(req, res) => {
+  const { todo } = req.body;
+  const newTodo = new Todo({ todo });
 
-res.status(201).json({message: 'Created'})
+  await newTodo
+    .save()
+    .then((newTodo) => console.log(newTodo))
+    .catch((error) => console.log(error));
 
+  res.status(201).json({ message: "Created" });
 };
 
-const readTodo = async(req, res) => {
-const todo = await Todo.find()    
-res.status(200).json({todo})
+const readTodo = async (req, res) => {
+  const todo = await Todo.find();
+  res.status(200).json({ todo });
 };
 
 const updateTodo = async (req, res) => {
-const {id} = req.params
-await Todo.findByIdAndUpdate(id, req.body)
-res.status(202).json({message: 'Updated'})
-
+  const { id } = req.params;
+  await Todo.findByIdAndUpdate(id, req.body);
+  res.status(202).json({ message: "Updated" });
 };
 
 const deleteTodo = async (req, res) => {
-    const {id} = req.params
-    await Todo.findByIdAndDelete(id)
-    res.status(204).json({message: 'Deleted'})
+  const { id } = req.params;
+  await Todo.findByIdAndDelete(id);
+  res.status(204).json({ message: "Deleted" });
 };
 
 export { createTodo, readTodo, updateTodo, deleteTodo };
-
